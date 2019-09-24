273b25e29fb120b4c308284119771048137ea2bb
Hiroki Nomoto <34984352+matbahasa@users.noreply.github.com>
2019-09-24 22:02:04 +0900

    Update check_tokens.py
    Output dalam format MALINDO Morph untuk memudahkan penambahan garis baru.

--- check_tokens.py
+++ check_tokens.py
@@ -39,11 +39,11 @@ for filename in glob.glob(os.path.join(path, '*.txt')):
             tokens.append(x)
 
 #remove all punctuations
-tokens = [re.sub("[\>\<\/\#\”\“\'\`\(\)\:\;\!\?\"\,\s\.\[\]]+", "", w) for w in tokens]
+tokens = [re.sub("[\>\<\#\”\“\'\`\(\)\:\;\!\?\"\,\s\.\[\]]+", "", w) for w in tokens] #Masalah: "menunggu/stesen" menjadi satu kata "menunggustesen" > "/" dibuang 
 tokens = [t for t in tokens if t != ""] #supaya "" tidak akan dihitung nanti
 
 #open MALINDO Morph and make a list of all surface forms
-m = codecs.open('/home/david/MALINDO_Morph/malindo_dic_20180817.tsv', encoding='utf-8', mode='r')
+m = codecs.open('/home/david/MALINDO_Morph/malindo_dic_20190923.tsv', encoding='utf-8', mode='r')
 katakata = []
 for baris in m.readlines():
     kata = baris.strip().split('\t')
@@ -72,3 +72,49 @@ copy = sorted(copy, reverse=True)
 #print each token and its frequency
 for k in copy:
     print ("%s\t%d" %(k[1], k[0]))
+
+
+#print each token in the MALINDO Morph format
+#analyse using MALINDO Morph morphological analyser
+import morph_analyzer as ma
+from pickle import load
+from spellchecker import SpellChecker
+
+def analisis(token, rootlist, Indo = False):
+    cand = ma.morph(token, rootlist)
+    rt,s,pfx,sfx,cfx,rdp = ([],token,[],[],[],[])
+    for c in cand:
+        if c[0] not in rt:
+            rt.append(c[0])
+        if c[2] not in pfx:
+            pfx.append(c[2])
+        if c[3] not in sfx:
+            sfx.append(c[3])
+        if c[4] not in cfx:
+            cfx.append(c[4])
+        if c[5] not in rdp:
+            rdp.append(c[5])
+    rt = "@".join(rt)
+    pfx = "@".join(pfx)
+    sfx = "@".join(sfx)
+    cfx = "@".join(cfx)
+    rdp = "@".join(rdp)
+    return rt,s,pfx,sfx,cfx,rdp
+
+with open("rootlist.pkl", "rb") as f:
+    rootlist = load(f)
+out = open("add2malindo.tsv", "w", encoding="utf-8")
+c = 9135 # counter utk nombor siri
+spell = SpellChecker()
+for t in diff:
+    r,s,pfx,sfx,cfx,rdp = analisis(t, rootlist)
+    print("ec-4{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\tLain\t{2}\t{2}".format(
+            str(c),r,t,pfx,sfx,cfx,rdp), file=out) # Tukar "Lain" kpd nama sumber yg sesuai
+# Untuk abaikan bahasa Inggeris
+#for t in diff:
+#    if spell.unknown([t]): #abaikan kalau bahasa Inggeris
+#        r,s,pfx,sfx,cfx,rdp = analisis(t, rootlist)
+#        print("ec-48{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\tMelayu-Sabah\t{2}\t{2}".format(
+#                str(c),r,t,pfx,sfx,cfx,rdp), file=out)
+    c += 1
+out.close()
